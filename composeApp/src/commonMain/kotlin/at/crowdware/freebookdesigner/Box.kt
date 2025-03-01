566b9d705ec3698870845cd8f431d0a5f0e82fca
Artanidos <japp.olaf@gmail.com>
2025-03-01 15:57:10 +0100

    refactoring

---
+++ composeApp/src/commonMain/kotlin/at/crowdware/freebookdesigner/Box.kt
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2025 CrowdWare
+ *
+ * This file is part of FreeBookDesigner.
+ *
+ *  FreeBookDesigner is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  FreeBookDesigner is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with FreeBookDesigner.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package at.crowdware.freebookdesigner
+
+// This is a test file to verify imports from NoCodeLib
